533fce0194df7116d0b4164f8bda1040143c0491
lamvv <lamvv9x@gmail.com>
2016-10-18 01:10:59 +0700

    update setting

--- app/src/main/java/io/github/lamvv/yboxnews/view/activity/ArticleActivity.java
+++ app/src/main/java/io/github/lamvv/yboxnews/view/activity/ArticleActivity.java
@@ -1,12 +1,5 @@
 package io.github.lamvv.yboxnews.view.activity;
 
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.LabeledIntent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,8 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -28,11 +19,7 @@ import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.github.lamvv.yboxnews.R;
-import io.github.lamvv.yboxnews.constant.BuildConfig;
 import io.github.lamvv.yboxnews.iml.GetArticleDetailTaskCompleteListener;
 import io.github.lamvv.yboxnews.model.Article;
 import io.github.lamvv.yboxnews.util.GetArticleDetailTask;
@@ -48,8 +35,6 @@ public class ArticleActivity extends AppCompatActivity implements GetArticleDeta
     protected int typeHomeMenu;
     private WebView mWebView;
     private ShareButton btnShareFacebook;
-    String detail;
-    Article article;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,7 +60,7 @@ public class ArticleActivity extends AppCompatActivity implements GetArticleDeta
 
         //get data send from fragments
         Bundle bundle = getIntent().getExtras();
-        article = (Article) bundle.getSerializable("article");
+        Article article = (Article) bundle.getSerializable("article");
 
         //webView
         mWebView = (WebView) findViewById(R.id.wvArticle);
@@ -86,7 +71,7 @@ public class ArticleActivity extends AppCompatActivity implements GetArticleDeta
 
 
         //launch task get detail article
-        detail = article.getLinks().getDetail();
+        String detail = article.getLinks().getDetail();
         if(detail != null)
             launchGetDetailTask(detail);
 
@@ -102,104 +87,6 @@ public class ArticleActivity extends AppCompatActivity implements GetArticleDeta
                 .build();
         btnShareFacebook.setShareContent(shareLinkContent);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        handleItemMenuSelected(id);
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public Toolbar getToolbar() {
-        return this.mToolbar;
-    }
-
-    private void handleItemMenuSelected(int id){
-        switch (id){
-            case R.id.action_setting:
-                startActivity(new Intent(ArticleActivity.this, SettingActivity.class));
-                break;
-            case R.id.action_share:
-                Resources resources = getResources();
-                Intent emailIntent = new Intent();
-                emailIntent.setAction(Intent.ACTION_SEND);
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.app_name));
-                emailIntent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.get_app) + " " +
-                        BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
-                emailIntent.setType("text/plain");
-
-
-                PackageManager pm = getPackageManager();
-                Intent sendIntent = new Intent(Intent.ACTION_SEND);
-                sendIntent.setType("message/rfc822");
-
-                Intent openInChooser = Intent.createChooser(emailIntent, resources.getString(R.string.share_app));
-                List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
-                List<LabeledIntent> intentList = new ArrayList<>();
-                for (int i = 0; i < resInfo.size(); i++) {
-                    // Extract the label, append it, and repackage it in a LabeledIntent
-                    ResolveInfo ri = resInfo.get(i);
-                    String packageName = ri.activityInfo.packageName;
-                    if(packageName.contains("android.email")) {
-                        emailIntent.setPackage(packageName);
-                    } else if(packageName.contains("twitter") || packageName.contains("facebook") || packageName.contains("mms") || packageName.contains("android.gm")) {
-                        Intent intent = new Intent();
-                        intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
-                        intent.setAction(Intent.ACTION_SEND);
-                        intent.setType("text/plain");
-                        if(packageName.contains("twitter")) {
-                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_name) + " " + resources.getString(R.string.get_app) + " " +
-                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
-                        } else if(packageName.contains("facebook")) {
-                            // Warning: Facebook IGNORES our text. They say "These fields are intended for users to express themselves. Pre-filling these fields erodes the authenticity of the user voice."
-                            // One workaround is to use the Facebook SDK to post, but that doesn't allow the user to choose how they want to share. We can also make a custom landing page, and the link
-                            // will show the <meta content ="..."> text from that page with our link in Facebook.
-                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_name) + " " +
-                                    resources.getString(R.string.get_app) + " " +
-                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
-                        } else if(packageName.contains("mms")) {
-                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_name) + " " +
-                                    resources.getString(R.string.get_app) + " " +
-                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
-                        } else if(packageName.contains("android.gm")) { // If Gmail shows up twice, try removing this else-if clause and the reference to "android.gm" above
-                            intent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.app_name));
-                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.get_app) + " " +
-                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
-                            intent.setType("message/rfc822");
-                        }
-                        intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
-                    }
-                }
-                // convert intentList to array
-                LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
-                openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
-                startActivity(openInChooser);
-                break;
-            case R.id.action_rate:
-                try {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + BuildConfig.APP_PACKAGE_NAME)));
-                } catch (ActivityNotFoundException e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME)));
-                }
-                break;
-            case R.id.action_otherapp:
-                try {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://developer?id=" + BuildConfig.DEV_STORE_ID)));
-                } catch (ActivityNotFoundException e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(BuildConfig.PLAY_STORE_DEV_URL + BuildConfig.DEV_STORE_ID)));
-                }
-                break;
-        }
     }
 
     public void setTypeHomeMenu(int typeHomeMenu) {
--- app/src/main/java/io/github/lamvv/yboxnews/view/activity/SettingActivity.java
+++ app/src/main/java/io/github/lamvv/yboxnews/view/activity/SettingActivity.java
@@ -37,7 +37,7 @@ public class SettingActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     protected int typeHomeMenu;
     private Switch switchTheme;
-    private TextView tvLikeApp, tvInvite, tvNeedHelp, tvMoreApp;
+    private TextView tvLikeApp, tvRateApp, tvInvite, tvSendFriend, tvNeedHelp, tvSendEmail, tvMoreApp, tvCheckApps;
 
     SharedPreferences prefs; // declare the sharedPreference
     boolean value = false; // default value if no value was found
@@ -92,7 +92,78 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        tvRateApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + BuildConfig.APP_PACKAGE_NAME)));
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME)));
+                }
+            }
+        });
+
         tvInvite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Resources resources = getResources();
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+//                emailIntent.setAction(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.app_name));
+                emailIntent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.get_app) + " " +
+                        BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
+//                emailIntent.setType("message/rfc822");
+                emailIntent.setType("text/plain");
+
+                PackageManager pm = getPackageManager();
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.setType("message/rfc822");
+
+                Intent openInChooser = Intent.createChooser(emailIntent, resources.getString(R.string.share_app));
+                List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+                List<LabeledIntent> intentList = new ArrayList<>();
+                for (int i = 0; i < resInfo.size(); i++) {
+                    // Extract the label, append it, and repackage it in a LabeledIntent
+                    ResolveInfo ri = resInfo.get(i);
+                    String packageName = ri.activityInfo.packageName;
+                    if(packageName.contains("android.email")) {
+                        emailIntent.setPackage(packageName);
+                    } else if(packageName.contains("twitter") || packageName.contains("facebook") || packageName.contains("mms") || packageName.contains("android.gm")) {
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+                        intent.setAction(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        if(packageName.contains("twitter")) {
+                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_name) + " " + resources.getString(R.string.get_app) + " " +
+                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
+                        } else if(packageName.contains("facebook")) {
+                            // Warning: Facebook IGNORES our text. They say "These fields are intended for users to express themselves. Pre-filling these fields erodes the authenticity of the user voice."
+                            // One workaround is to use the Facebook SDK to post, but that doesn't allow the user to choose how they want to share. We can also make a custom landing page, and the link
+                            // will show the <meta content ="..."> text from that page with our link in Facebook.
+                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_name) + " " +
+                                    resources.getString(R.string.get_app) + " " +
+                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
+                        } else if(packageName.contains("mms")) {
+                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_name) + " " +
+                                    resources.getString(R.string.get_app) + " " +
+                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
+                        } else if(packageName.contains("android.gm")) { // If Gmail shows up twice, try removing this else-if clause and the reference to "android.gm" above
+                            intent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.app_name));
+                            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.get_app) + " " +
+                                    BuildConfig.PLAY_STORE_APP_URL + BuildConfig.APP_PACKAGE_NAME);
+                            intent.setType("message/rfc822");
+                        }
+                        intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+                    }
+                }
+                // convert intentList to array
+                LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
+                openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+                startActivity(openInChooser);
+            }
+        });
+
+        tvSendFriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Resources resources = getResources();
@@ -168,7 +239,34 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        tvSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSendMail = new Intent(Intent.ACTION_SEND);
+                intentSendMail.setType("*/*");
+                intentSendMail.putExtra(Intent.EXTRA_EMAIL, new String[]{"lamvv9x@gmail.com"});
+                intentSendMail.putExtra(Intent.EXTRA_SUBJECT, "Feedback to " + getResources().getString(R.string.app_name));
+                intentSendMail.putExtra(Intent.EXTRA_TEXT, "");
+                try {
+                    startActivity(intentSendMail);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(SettingActivity.this, getString(R.string.not_email), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         tvMoreApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://developer?id=" + BuildConfig.DEV_STORE_ID)));
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(BuildConfig.PLAY_STORE_DEV_URL + BuildConfig.DEV_STORE_ID)));
+                }
+            }
+        });
+
+        tvCheckApps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
@@ -184,9 +282,13 @@ public class SettingActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         switchTheme = (Switch)findViewById(R.id.switchTheme);
         tvLikeApp = (TextView)findViewById(R.id.likeApp);
+        tvRateApp = (TextView)findViewById(R.id.rateApp);
         tvInvite = (TextView)findViewById(R.id.invite);
+        tvSendFriend = (TextView)findViewById(R.id.sendFriend);
         tvNeedHelp = (TextView)findViewById(R.id.needHelp);
+        tvSendEmail = (TextView)findViewById(R.id.sendEmail);
         tvMoreApp = (TextView)findViewById(R.id.moreApp);
+        tvCheckApps = (TextView)findViewById(R.id.checkApps);
     }
 
     public void setTypeHomeMenu(int typeHomeMenu) {
