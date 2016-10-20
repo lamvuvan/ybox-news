4d0dd76bb53f57ee67dfb5b1ce4390e992d3ac4f
lamvv <lamvv9x@gmail.com>
2016-10-20 13:06:54 +0700

    fix anim for article detail activity

---
+++ app/src/main/java/io/github/lamvv/yboxnews/util/ScrollingFABBehavior.java
@@ -0,0 +1,40 @@
+package io.github.lamvv.yboxnews.util;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lamvu on 10/20/2016.
+ */
+
+public class ScrollingFABBehavior extends FloatingActionButton.Behavior {
+
+    private int toolbarHeight;
+
+    public ScrollingFABBehavior(Context context, AttributeSet attrs) {
+        super();
+        this.toolbarHeight = Utils.getToolbarHeight(context);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        return super.layoutDependsOn(parent, fab, dependency) || (dependency instanceof AppBarLayout);
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        boolean returnValue = super.onDependentViewChanged(parent, fab, dependency);
+        if (dependency instanceof AppBarLayout) {
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+            int fabBottomMargin = lp.bottomMargin;
+            int distanceToScroll = fab.getHeight() + fabBottomMargin;
+            float ratio = (float)dependency.getY()/(float)toolbarHeight;
+            fab.setTranslationY(-distanceToScroll * ratio);
+        }
+        return returnValue;
+    }
+}
---
+++ app/src/main/java/io/github/lamvv/yboxnews/util/Utils.java
@@ -0,0 +1,26 @@
+package io.github.lamvv.yboxnews.util;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+import io.github.lamvv.yboxnews.R;
+
+/**
+ * Created by lamvu on 10/20/2016.
+ */
+
+public class Utils {
+
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+
+    public static int getTabsHeight(Context context) {
+        return (int) context.getResources().getDimension(R.dimen.tabsHeight);
+    }
+}
